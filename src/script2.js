f5d0955390dcc2aa14b3e89fe072baec965f2594
tiennguyen00 <18521487@gm.uit.edu.vn>
2025-05-04 00:01:51 +0700

    init: add pedal models

---
+++ src/script2.js
@@ -0,0 +1,132 @@
+import GUI from "lil-gui";
+import * as THREE from "three/webgpu";
+import {
+  sin,
+  positionLocal,
+  time,
+  vec2,
+  vec3,
+  vec4,
+  uv,
+  uniform,
+  color,
+  fog,
+  rangeFogFactor,
+  pass,
+  renderOutput,
+} from "three/tsl";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { sobel } from "three/addons/tsl/display/SobelOperatorNode.js";
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI({
+  width: 400,
+});
+
+/**
+ * Loaders
+ */
+const loader = new GLTFLoader();
+loader.load("./pedals.glb", function (gltf) {
+  const object = gltf.scene;
+  object.scale.set(0.2, 0.2, 0.2);
+  scene.add(object);
+});
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+// const fogColor = uniform(color("#ffffff"));
+// scene.fogNode = fog(fogColor, rangeFogFactor(10, 15));
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  25,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 6;
+camera.position.y = 3;
+camera.position.z = 10;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGPURenderer({
+  canvas: canvas,
+  forceWebGL: false,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setClearColor(0xbdb3be);
+
+/**
+ * Post processing
+ */
+
+// postProcessing.outputNode = outputPass
+
+/**
+ * Dummy
+ */
+// Material
+const material = new THREE.MeshBasicNodeMaterial();
+
+// Uniforms
+const timeFrequency = uniform(0.5);
+const positionFrequency = uniform(2);
+const intensityFrequency = uniform(0.5);
+
+// Tweaks
+gui.add(timeFrequency, "value").min(0).max(5).name("timeFrequency");
+gui.add(positionFrequency, "value").min(0).max(5).name("positionFrequency");
+gui.add(intensityFrequency, "value").min(0).max(5).name("intensityFrequency");
+
+/**
+ * Animate
+ */
+const tick = () => {
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+};
+renderer.setAnimationLoop(tick);
