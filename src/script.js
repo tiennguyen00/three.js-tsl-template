7faf6b0dca62e17e7581ffe25c1becf01c895934
Tien Nguyen <nhtiendev@gmail.com>
2025-05-09 11:59:27 +0700

    initial commit

--- src/script.js
+++ src/script.js
@@ -1,131 +1,163 @@
-import GUI from 'lil-gui'
-import * as THREE from 'three/webgpu'
-import { sin, positionLocal, time, vec2, vec3, vec4, uv, uniform, color, fog, rangeFogFactor, pass, renderOutput } from 'three/tsl'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import { sobel } from 'three/addons/tsl/display/SobelOperatorNode.js';
+import GUI from "lil-gui";
+import * as THREE from "three/webgpu";
+
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import VirtualScroll from "virtual-scroll";
+import {
+  Fn,
+  positionLocal,
+  modelWorldMatrix,
+  length,
+  pow,
+  float,
+} from "three/tsl";
 
 /**
  * Base
  */
 // Debug
 const gui = new GUI({
-    width: 400
-})
+  width: 400,
+});
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
-const fogColor = uniform(color('#ffffff'))
-scene.fogNode = fog(fogColor, rangeFogFactor(10, 15))
+const scene = new THREE.Scene();
+
+// Loader
+const loader = new GLTFLoader();
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(25, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 6
-camera.position.y = 3
-camera.position.z = 10
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  70,
+  sizes.width / sizes.height,
+  0.01,
+  100
+);
+camera.position.set(0, 0, 8);
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+// const controls = new OrbitControls(camera, canvas);
+// controls.enableDamping = true;
+
+// Scroll
+const scroller = new VirtualScroll();
+let scrollPosition = 0;
+scroller.on((event) => {
+  scrollPosition = event.y * 0.001;
+});
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGPURenderer({
-    canvas: canvas,
-    forceWebGL: false
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-renderer.setClearColor(fogColor.value)
-
-/**
- * Post processing
- */
-const postProcessing = new THREE.PostProcessing(renderer)
-postProcessing.outputColorTransform = false
-
-const scenePass = pass(scene, camera)
-const outputPass = renderOutput(scenePass)
-
-postProcessing.outputNode = sobel(outputPass)
-// postProcessing.outputNode = outputPass
-
+  canvas: canvas,
+  forceWebGL: false,
+  antialias: true,
+  transparent: true,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setClearColor(0x000000);
 
 /**
  * Dummy
  */
-// Material
-const material = new THREE.MeshBasicNodeMaterial()
-
-// Uniforms
-const timeFrequency = uniform(0.5)
-const positionFrequency = uniform(2)
-const intensityFrequency = uniform(0.5)
-
-// Position
-const oscillation = sin(time.mul(timeFrequency).add(positionLocal.y.mul(positionFrequency))).mul(intensityFrequency)
-material.positionNode = vec3(
-    positionLocal.x.add(oscillation),
-    positionLocal.y,
-    positionLocal.z
-)
-
-// Color
-material.colorNode = vec4(
-    uv().mul(vec2(32, 8)).fract(),
-    1,
-    1
-)
 
 // Mesh
-const torusKnot = new THREE.Mesh(new THREE.TorusKnotGeometry(1, 0.35, 128, 32), material)
-scene.add(torusKnot)
 
-// Tweaks
-gui.add(timeFrequency, 'value').min(0).max(5).name('timeFrequency')
-gui.add(positionFrequency, 'value').min(0).max(5).name('positionFrequency')
-gui.add(intensityFrequency, 'value').min(0).max(5).name('intensityFrequency')
+let t1 = new THREE.TextureLoader().load(
+  `https://picsum.photos/seed/1/300/200.webp`
+);
+let t2 = new THREE.TextureLoader().load(
+  `https://picsum.photos/seed/2/300/200.webp`
+);
+let t3 = new THREE.TextureLoader().load(
+  `https://picsum.photos/seed/3/300/200.webp`
+);
+let t4 = new THREE.TextureLoader().load(
+  `https://picsum.photos/seed/4/300/200.webp`
+);
+const tts = [t1, t2, t3, t4];
+tts.forEach((t) => {
+  t.colorSpace = THREE.SRGBColorSpace;
+});
+let slides = [];
+let mat = new THREE.MeshBasicNodeMaterial();
+mat.positionNode = Fn(() => {
+  const position = positionLocal.xyz.toVar();
+  const positionWorld = modelWorldMatrix.mul(position);
+
+  let distanceFromCenter = length(positionWorld.x);
+
+  position.y.mulAssign(float(1).add(pow(distanceFromCenter.mul(0.1), 2)));
+
+  return position;
+})();
+
+const geometry = new THREE.PlaneGeometry(8, 4.5, 10, 10);
+for (let i = 0; i < 6; i++) {
+  const matClone = mat.clone();
+  matClone.map = tts[i % tts.length];
+  const slide = new THREE.Mesh(geometry, matClone);
+  slide.position.set(i * 8, 0, 0);
+  scene.add(slide);
+  slides.push({
+    mesh: slide,
+    index: i,
+    originalPosition: slide.position.x,
+  });
+}
 
 /**
  * Animate
  */
-const tick = () =>
-{
-    // Update controls
-    controls.update()
+let radius = 12;
+let dist = 8;
+const tick = () => {
+  // Update controls
+  //   controls.update();
+  slides.forEach((object) => {
+    object.mesh.position.x = object.originalPosition + scrollPosition;
+    const angle = object.mesh.position.x * 0.08;
+    let x = Math.cos(angle) * radius;
+    let z = Math.sin(angle) * radius;
 
-    // Render
-    // renderer.renderAsync(scene, camera)
-    postProcessing.renderAsync(scene, camera)
-}
-renderer.setAnimationLoop(tick)
+    // object.mesh.position.set(x, 0, z);
+    // object.mesh.rotation.set(0, -angle - Math.PI * 0.5, 0);
+  });
+
+  // Render
+  renderer.renderAsync(scene, camera);
+};
+
+renderer.setAnimationLoop(tick);
